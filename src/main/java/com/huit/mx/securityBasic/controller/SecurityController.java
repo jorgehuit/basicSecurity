3981ab639acb8a3b364cc0e837359bba5f3519b2
Jorge Gómez Veláquez <g.jorgehuit@gmail.com>
2019-08-26 17:00:17 -0500

    Se agrgega test con headers de autentication

--- src/main/java/com/huit/mx/securityBasic/controller/SecurityController.java
+++ src/main/java/com/huit/mx/securityBasic/controller/SecurityController.java
@@ -1,6 +1,6 @@
 package com.huit.mx.securityBasic.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.huit.mx.securityBasic.dto.Greet;
@@ -8,7 +8,7 @@ import com.huit.mx.securityBasic.dto.Greet;
 @RestController
 public class SecurityController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	Greet greet() {
 		return new Greet("Hello World!");
 	}
--- src/test/java/com/huit/mx/securityBasic/SecurityBasicApplicationTests.java
+++ src/test/java/com/huit/mx/securityBasic/SecurityBasicApplicationTests.java
@@ -1,16 +1,43 @@
 package com.huit.mx.securityBasic;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.huit.mx.securityBasic.dto.Greet;
+
+import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log4j
 public class SecurityBasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testSecureService(){
+		log.info("Colocamos el usuario y pass");
+		String plainCreds = "huit:huit123";
+		HttpHeaders httpHeaders = new HttpHeaders();
+		log.info("Agregamos el objeto Authorization con el token");
+		httpHeaders.add("Authorization", "Basic " +  new String (Base64.encode(plainCreds.getBytes())));
+		HttpEntity<String> request = new HttpEntity<>(httpHeaders);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<Greet> response = restTemplate.exchange("http://localhost:8080/", HttpMethod.GET, request, Greet.class);
+		Assert.assertEquals("Hello World!", response.getBody().getMessage());
+		
 	}
 
 }
